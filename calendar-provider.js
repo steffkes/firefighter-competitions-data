17ab16df585ac483baca7ba8a493fb11c8c762cc
steffkes <stefan@gelungen.es>
2023-01-26 21:24:34 +0100

    use competition id as event identifier

--- calendar-provider.js
+++ calendar-provider.js
@@ -22,6 +22,7 @@ module.exports = Object.fromEntries(
 
         for (const competition of filtered) {
           calendar.createEvent({
+            id: competition.id,
             status: ["CONFIRMED", "TENTATIVE"][+competition.date.is_draft],
             start: competition.date.start,
             end: competition.date.end,

bc73ffd42b6ae15e2425508725eb6ef739eef24e
steffkes <stefan@gelungen.es>
2023-06-11 22:35:58 +0200

    fix path for competition provider

--- server/api/competitions.js
+++ server/api/competitions.js
@@ -1,4 +1,4 @@
 import { defineEventHandler } from "h3"; // needed for test
-import competitionProvider from "../../../competition-provider";
+import competitionProvider from "@/competition-provider";
 
 export default defineEventHandler(async (event) => await competitionProvider());

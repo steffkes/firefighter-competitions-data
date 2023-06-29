6178b166988955c26d5ed66417d38be1b5b89132
steffkes <stefan@gelungen.es>
2023-06-29 14:28:20 +0200

    fix event duration in calendar
    end date needs to be adjusted by +1 if competition are longer than one
    single day.

--- calendar-provider.js
+++ calendar-provider.js
@@ -1,5 +1,6 @@
 const ical = require("ical-generator");
 const competitionProvider = require("./competition-provider");
+import { add } from "date-fns";
 
 module.exports = Object.fromEntries(
   [["FCC", "FSR"], ["FCC"], ["FSR"]].map((variant) => {
@@ -28,12 +29,17 @@ module.exports = Object.fromEntries(
           if (competition.date.is_canceled) {
             status = "CANCELLED";
           }
+
           calendar.createEvent({
             id: competition.id,
             url: competition.url,
             status,
             start: competition.date.start,
-            end: competition.date.end,
+            // competitions over multiple days need the end date adjusted by +1
+            // otherwise the last days is skipped in calendars
+            end: add(competition.date.end, {
+              days: competition.date.start < competition.date.end ? 1 : 0,
+            }),
             allDay: true,
             summary: "👨‍🚒 " + competition.kind + ": " + competition.name,
             location: {

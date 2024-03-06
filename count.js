610ef6187b2831cf031d0085876ce5a73af6b5c9
steffkes <stefan@gelungen.es>
2024-03-06 17:47:24 +0100

    improve count handling
    make behaviour more dynamic, don't rely on static list of files

--- count.js
+++ count.js
@@ -1,24 +1,13 @@
-import { count as countRecbbcQl0KFKj7Ox4 } from "./server/api/competitions/recbbcQl0KFKj7Ox4.json.js";
-import { count as countRecNdFl6cR1xA5l4p } from "./server/api/competitions/recNdFl6cR1xA5l4p.json.js";
-import { count as countRecqjh5V3DUAzgLYQ } from "./server/api/competitions/recqjh5V3DUAzgLYQ.json.js";
-import { count as countRecRqH7LW1AQQ66FR } from "./server/api/competitions/recRqH7LW1AQQ66FR.json.js";
-import { count as countRecVzAj3TgO0th7JC } from "./server/api/competitions/recVzAj3TgO0th7JC.json.js";
-import { count as countRecxAxWccPrMWcVVv } from "./server/api/competitions/recxAxWccPrMWcVVv.json.js";
-import { count as countRecyC5LmxecehTxWD } from "./server/api/competitions/recyC5LmxecehTxWD.json.js";
-
-const data = {
-  recbbcQl0KFKj7Ox4: await countRecbbcQl0KFKj7Ox4(),
-  recNdFl6cR1xA5l4p: await countRecNdFl6cR1xA5l4p(),
-  recqjh5V3DUAzgLYQ: await countRecqjh5V3DUAzgLYQ(),
-  recRqH7LW1AQQ66FR: await countRecRqH7LW1AQQ66FR(),
-  recVzAj3TgO0th7JC: await countRecVzAj3TgO0th7JC(),
-  recxAxWccPrMWcVVv: await countRecxAxWccPrMWcVVv(),
-  recyC5LmxecehTxWD: await countRecyC5LmxecehTxWD(),
-};
+import glob from "fast-glob";
+import { basename } from "path";
 
 const date = new Date();
 const str = (object) => JSON.stringify({ date: date.toISOString(), ...object });
 
-for (const [competition, count] of Object.entries(data)) {
-  console.log(str({ competition, count }));
+const extension = ".json.js";
+for (const path of await glob(["./server/api/competitions/*" + extension])) {
+  const { count } = await import(path);
+  console.log(
+    str({ competition: basename(path, extension), count: await count() })
+  );
 }

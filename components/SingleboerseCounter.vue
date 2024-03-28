9139f556a14fdbe7fcdcf602c3fc30b1e3102076
steffkes <stefan@gelungen.es>
2024-03-28 13:58:42 +0100

    integrate singlebörse

---
+++ components/SingleboerseCounter.vue
@@ -0,0 +1,15 @@
+<template>
+  <span
+    class="tag is-link"
+    :title="'aktuell ' + count + ' Gesuche'"
+    style="cursor: help"
+    >🤚 {{ count }}</span
+  >
+</template>
+
+<script setup>
+const { id } = defineProps(["id"]);
+const { data: count } = await useFetch(
+  "https://match-me-if-you-can.vercel.app/api/" + id + ".json"
+);
+</script>

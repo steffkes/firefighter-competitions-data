26b1f5dba156025b87f57cf2df3e989d2be1ab42
steffkes <stefan@gelungen.es>
2024-05-17 11:16:06 +0200

    fix participants for berlin

--- analysis/spider/240504_reclRpQWg31zYe1uI_berlin.py
+++ analysis/spider/240504_reclRpQWg31zYe1uI_berlin.py
@@ -1,27 +1,55 @@
 import scrapy
 from datetime import datetime
+from scrapy.utils.iterators import csviter
+from util import JsonItemExporter, ParticipantItem, ResultItem
 
 
 class Spider(scrapy.spiders.CSVFeedSpider):
     name = __name__
     race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
     competition_id = __name__.split("_")[1]
+    ident = __name__[0:24]
 
     tmp_bib = 1
     delimiter = ";"
 
     custom_settings = {
+        "FEED_EXPORTERS": {"starter": JsonItemExporter},
         "FEEDS": {
+            "../data/teams/%(ident)s.json": {
+                "format": "starter",
+                "encoding": "utf8",
+                "overwrite": True,
+                "item_classes": [ParticipantItem],
+            },
             "data/teams/%(name)s.jsonl": {
                 "format": "jsonlines",
                 "encoding": "utf8",
                 "overwrite": True,
-            }
-        }
+                "item_classes": [ResultItem],
+            },
+        },
+        "EXTENSIONS": {
+            "scrapy.extensions.telnet.TelnetConsole": None,
+        },
     }
 
     def start_requests(self):
-        yield scrapy.Request("file:///app/spider/data/240504_berlin.csv")
+        yield scrapy.Request(
+            "file:///app/analysis/spider/data/240504_berlin_starter.csv",
+            callback=self.parse_starters,
+        )
+        yield scrapy.Request("file:///app/analysis/spider/data/240504_berlin.csv")
+
+    def parse_starters(self, response):
+        fixName = lambda name: " ".join(reversed(name.split(", ")))
+        for row in csviter(
+            response, self.delimiter, self.headers, quotechar=self.quotechar
+        ):
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted(map(fixName, [row["name1"], row["name2"]])),
+            )
 
     def parse_row(self, response, row):
         names = sorted(
@@ -31,14 +59,14 @@ class Spider(scrapy.spiders.CSVFeedSpider):
             )
         )
 
-        yield {
-            "date": self.race_date,
-            "competition_id": self.competition_id,
-            "bib": self.tmp_bib,
-            "type": "MPA",
-            "duration": "00:" + row["Time"][0:7],
-            "category": {"Ladies": "W", "Mix": "X"}.get(row["Category"].strip(), "M"),
-            "names": names,
-        }
+        yield ResultItem(
+            date=self.race_date,
+            competition_id=self.competition_id,
+            bib=self.tmp_bib,
+            type="MPA",
+            duration="00:" + row["Time"][0:7],
+            category={"Ladies": "W", "Mix": "X"}.get(row["Category"].strip(), "M"),
+            names=names,
+        )
 
         self.tmp_bib = self.tmp_bib + 1

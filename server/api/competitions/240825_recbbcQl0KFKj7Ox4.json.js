30431d4d3640ee89f34b9b706d400d8d7a64188b
steffkes <stefan@gelungen.es>
2024-05-17 13:52:50 +0200

    cleanup competitions
    data is collected from analysis now

--- server/api/competitions/240825_recbbcQl0KFKj7Ox4.json.js
+++ server/api/competitions/240825_recbbcQl0KFKj7Ox4.json.js
@@ -1,36 +1,3 @@
-import { ofetch } from "ofetch";
-
-const reverseName = (name) => name.split(", ").reverse().join(" ");
-
-const tmp = (
-  await Promise.allSettled(
-    [5, 6].map(
-      async (contest) =>
-        new Promise(async (resolve) => {
-          const { data } = await ofetch(
-            "https://my1.raceresult.com/271676/RRPublish/data/list",
-            {
-              query: {
-                key: "b43c7682c8720d9ae73fe4b5f1939e3b",
-                contest,
-                listname: "07 - Teilnehmer|Teilnehmerliste ABC Team",
-              },
-            }
-          );
-          return resolve(Object.values(data)[0]);
-        })
-    )
-  )
-)
-  .flatMap(({ value }) => value)
-  .map(([_bib, _id, _team, names, _category]) =>
-    names.split(" / ").map(reverseName)
-  );
-
-export const teams = () => tmp;
-export const participants = () => teams().flat();
-export const count = () => participants().length;
-
 import { defineEventHandler } from "h3";
 import data from "@/data/teams/240825_recbbcQl0KFKj7Ox4.json" assert { type: "json" };
 

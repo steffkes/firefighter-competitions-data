b36d239a5beeeed743d2ad47dbb19e004e5a18ef
steffkes <stefan@gelungen.es>
2025-01-14 15:31:20 +0100

    collect age_group and rank for zagreb

--- spider/231202_recKKmm4DSwQY4UWj_zagreb.py
+++ spider/231202_recKKmm4DSwQY4UWj_zagreb.py
@@ -1,6 +1,12 @@
 import scrapy
 from datetime import datetime
-from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
+from util import (
+    JsonItemExporter,
+    JsonLinesItemExporter,
+    ParticipantItem,
+    ResultItem,
+    ResultRankItem,
+)
 
 
 class Spider(scrapy.Spider):
@@ -70,8 +76,9 @@ class Spider(scrapy.Spider):
     def parse(self, response, type):
 
         for entry in response.css("table tbody tr"):
+            cells = entry.css("td")
             [
-                _,
+                total_rank,
                 bib,
                 name,
                 category,
@@ -82,14 +89,35 @@ class Spider(scrapy.Spider):
                 _pace,
                 _speed,
                 _rank,
-            ] = [cell.css("::text").get() for cell in entry.css("td")]
+            ] = [cell.css("::text").get() for cell in cells]
+            [
+                _category,
+                category_rank,
+                age_group_info,
+            ] = (
+                cells[10].css("span::text").getall()
+            )
+
+            [
+                _category,
+                category_rank,
+                age_group_info,
+            ] = [subcell.css("::text").get() for subcell in cells[10].css("span")]
+
+            [age_group_rank, age_group] = age_group_info.split(" - ")
 
             yield ResultItem(
                 date=self.race_date,
                 competition_id=self.competition_id,
                 type=type,
-                category=category,
+                category={"F": "W"}.get(category, category),
                 duration="00:" + (("0" + duration)[-9:])[:7],
                 names=[name],
                 bib=bib.strip(),
+                age_group=age_group.replace("Ž", "W"),
+                rank=ResultRankItem(
+                    total=int(total_rank),
+                    category=int(category_rank),
+                    age_group=int(age_group_rank),
+                ),
             )

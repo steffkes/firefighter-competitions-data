4cdd50dba3ad81106a0fda1c051ab7fa4260895f
steffkes <stefan@gelungen.es>
2025-02-01 08:46:09 +0100

    participants for zwickau

---
+++ spider/250413_recDQgPZf3fKZwQV1_zwickau.py
@@ -0,0 +1,56 @@
+import scrapy
+from datetime import datetime
+from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
+
+
+class Spider(scrapy.Spider):
+    name = __name__
+    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
+    competition_id = __name__.split("_")[1]
+    ident = __name__[0:24]
+
+    race_id = "322261"
+    race_key = "08e70f5a3af5efda8835286bd20fc249"
+
+    custom_settings = {
+        "FEED_EXPORTERS": {
+            "starter": JsonItemExporter,
+            "results": JsonLinesItemExporter,
+        },
+        "FEEDS": {
+            "data/participants/%(ident)s.json": {
+                "format": "starter",
+                "encoding": "utf8",
+                "overwrite": True,
+                "item_classes": [ParticipantItem],
+            },
+            "data/results/%(name)s.jsonl": {
+                "format": "results",
+                "encoding": "utf8",
+                "overwrite": True,
+                "item_classes": [ResultItem],
+            },
+        },
+        "EXTENSIONS": {
+            "scrapy.extensions.telnet.TelnetConsole": None,
+        },
+    }
+
+    def start_requests(self):
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "Teilnehmerlisten|Teilnehmerliste Feuerwehr",
+                "contest": "2",
+            },
+            callback=self.parse_starters,
+        )
+
+    def parse_starters(self, response):
+        for [_bib, _team, _gender, names] in response.json()["data"]:
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted(names.split(" / ")),
+            )

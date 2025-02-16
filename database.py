c2c5582f8ccfe0c7846ef2b3656290fb64c1a301
steffkes <stefan@gelungen.es>
2025-02-16 12:07:15 +0100

    publish results to database
    run once every hour, for now

---
+++ database.py
@@ -0,0 +1,78 @@
+from psycopg2 import pool, extras
+from itertools import repeat
+from pathlib import Path
+import jsonlines
+import json
+import os
+
+connection_pool = pool.SimpleConnectionPool(1, 10, os.getenv("DB_DSN"))
+
+conn = connection_pool.getconn()
+cur = conn.cursor()
+
+cur.execute("DROP TABLE IF EXISTS participants")
+cur.execute(
+    """
+CREATE TABLE participants (
+  competition_id TEXT,
+  teams VARCHAR[]
+)
+"""
+)
+
+
+def loaderFn(path):
+    try:
+        with path.open() as handle:
+            return (path, json.load(handle))
+    except:
+        return None
+
+
+for path, competition in filter(
+    None, map(loaderFn, sorted(Path("data/participants").glob("*.json")))
+):
+    print(path)
+    data = list(
+        map(
+            lambda teams, competition_id: (competition_id, teams),
+            competition["teams"],
+            repeat(competition["competition_id"]),
+        )
+    )
+    extras.execute_values(cur, "INSERT INTO participants VALUES %s", data)
+
+cur.execute("DROP TABLE IF EXISTS results")
+cur.execute(
+    """
+CREATE TABLE results (
+  competition_id TEXT,
+  duration TEXT,
+  type TEXT,
+  names VARCHAR[]
+)
+"""
+)
+
+for path in sorted(
+    filter(lambda path: path.stat().st_size > 1, Path("data/results").glob("*.jsonl"))
+):
+    print(path)
+    with jsonlines.open(path.resolve()) as reader:
+        data = list(
+            map(
+                lambda record: (
+                    record["competition_id"],
+                    record["duration"],
+                    record["type"],
+                    record["names"],
+                ),
+                reader,
+            )
+        )
+        extras.execute_values(cur, "INSERT INTO results VALUES %s", data)
+
+conn.commit()
+
+cur.close()
+connection_pool.closeall()

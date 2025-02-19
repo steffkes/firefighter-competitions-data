562bf99d6513d93c8c9b85cf0e11ca5b92cfcfec
steffkes <stefan@gelungen.es>
2025-02-19 11:08:13 +0100

    add participants for koeln 2025

---
+++ spider/250803_rec0hVWR2BM4b4Ltn_koeln.py
@@ -0,0 +1,70 @@
+import scrapy
+from datetime import datetime
+import re
+import itertools
+import string
+import requests
+from util import (
+    JsonItemExporter,
+    JsonLinesItemExporter,
+    ParticipantItem,
+    ResultItem,
+    ResultRankItem,
+)
+
+
+class Spider(scrapy.Spider):
+    name = __name__
+    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
+    competition_id = __name__.split("_")[1]
+    ident = __name__[0:24]
+
+    race_id = "323602"
+    race_key = "77bdc85dcb9f1b7a137834310113b943"
+
+    custom_settings = {
+        "FEED_EXPORTERS": {
+            "starter": JsonItemExporter,
+            "results": JsonLinesItemExporter,
+        },
+        "FEEDS": {
+            "data/participants/%(ident)s.json": {
+                "format": "starter",
+                "encoding": "utf8",
+                "overwrite": True,
+                "item_classes": [ParticipantItem],
+            },
+            "data/results/%(name)s.jsonl": {
+                "format": "results",
+                "encoding": "utf8",
+                "overwrite": True,
+                "item_classes": [ResultItem],
+            },
+        },
+        "EXTENSIONS": {
+            "scrapy.extensions.telnet.TelnetConsole": None,
+        },
+    }
+
+    def start_requests(self):
+        for contest in [5, 6]:
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                formdata={
+                    "key": self.race_key,
+                    "listname": "07 - Teilnehmer|Teilnehmerliste ABC Team",
+                    "contest": str(contest),
+                },
+                callback=self.parse_starters,
+            )
+
+    def parse_starters(self, response):
+        fixName = lambda name: " ".join(reversed(list(map(str.strip, name.split(",")))))
+
+        data = list(response.json()["data"].values())[0]
+        for [_bib, _id, _team, names, _category] in data:
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted(map(fixName, names.split(" / "))),
+            )

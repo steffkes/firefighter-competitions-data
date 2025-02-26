808fdfeeec83abe9403b9b5fa4b6f8a90072f7fc
steffkes <stefan@gelungen.es>
2025-02-26 11:40:38 +0100

    refactor spider for riva del garda 2024

--- spider/241005_recCUVH0nuqh6lcjO_rivadelgarda.py
+++ spider/241005_recCUVH0nuqh6lcjO_rivadelgarda.py
@@ -1,47 +1,13 @@
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
 import scrapy
-from datetime import datetime
-from util import (
-    JsonItemExporter,
-    JsonLinesItemExporter,
-    ParticipantItem,
-    ResultItem,
-    ResultRankItem,
-)
 
 nameMappings = {"Hindelang Günther": "Günther Hindelang"}
 
 
-class Spider(scrapy.Spider):
+class CompetitionSpider(Spider):
     name = __name__
-    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
-    competition_id = __name__.split("_")[1]
-    ident = __name__[0:24]
 
     ranks = {"total": 1, "category": {"M": 1, "W": 1}}
-
-    custom_settings = {
-        "FEED_EXPORTERS": {
-            "starter": JsonItemExporter,
-            "results": JsonLinesItemExporter,
-        },
-        "FEEDS": {
-            "data/participants/%(ident)s.json": {
-                "format": "starter",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ParticipantItem],
-            },
-            "data/results/%(name)s.jsonl": {
-                "format": "results",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ResultItem],
-            },
-        },
-        "EXTENSIONS": {
-            "scrapy.extensions.telnet.TelnetConsole": None,
-        },
-    }
 
     def start_requests(self):
         yield scrapy.FormRequest(

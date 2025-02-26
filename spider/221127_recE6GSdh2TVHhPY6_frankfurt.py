f38d65c4e4eeffd80dfdb3a63d084a4117b3b9e5
steffkes <stefan@gelungen.es>
2025-02-27 00:45:39 +0100

    improve results for frankfurt 2022

--- spider/221127_recE6GSdh2TVHhPY6_frankfurt.py
+++ spider/221127_recE6GSdh2TVHhPY6_frankfurt.py
@@ -1,35 +1,29 @@
-import scrapy
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
 from datetime import datetime
-from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
+import scrapy
+import itertools
+import re
 
 
-class Spider(scrapy.Spider):
+class CompetitionSpider(Spider):
     name = __name__
-    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
-    competition_id = __name__.split("_")[1]
-    ident = __name__[0:24]
 
     race_id = "210595"
     race_key = "c1080e06f270bfa9c6172101d8fb33bb"
 
-    custom_settings = {
-        "FEED_EXPORTERS": {
-            "results": JsonLinesItemExporter,
-        },
-        "FEEDS": {
-            "data/results/%(name)s.jsonl": {
-                "format": "results",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ResultItem],
-            },
-        },
-        "EXTENSIONS": {
-            "scrapy.extensions.telnet.TelnetConsole": None,
-        },
-    }
-
     def start_requests(self):
+        for contest in [3, 4]:
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                formdata={
+                    "key": self.race_key,
+                    "listname": "Teilnehmerlisten|TN Startzeit",
+                    "contest": str(contest),
+                },
+                callback=self.parse_starters,
+            )
+
         for contest, competition_type, data_key in [
             (3, "OPA", "#1_messeTurm FFC FIGHTER"),
             (4, "MPA", "#1_messeTurm FFC ELITE"),
@@ -39,7 +33,7 @@ class Spider(scrapy.Spider):
                 url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
                 formdata={
                     "key": self.race_key,
-                    "listname": "Ergebnislisten|EG ZIEL M/W",
+                    "listname": "Ergebnislisten|EG ZIEL Teamwertung FF&RDC",
                     "contest": str(contest),
                 },
                 cb_kwargs={
@@ -48,34 +42,103 @@ class Spider(scrapy.Spider):
                 },
             )
 
+    def parse_starters(self, response):
+        fixName = lambda name: " ".join(reversed(list(map(str.strip, name.split(",")))))
+
+        starters = []
+        for [
+            _id,
+            _empty,
+            bib,
+            name,
+            _gender,
+            _category,
+            _team,
+            _nationality,
+            _competition,
+            _empty,
+        ] in response.json()["data"]:
+            starters.append((bib[0:-2], fixName(name)))
+
+        for bib, items in itertools.groupby(starters, lambda item: item[0]):
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted(map(lambda item: item[1], items)),
+            )
+
     def parse(self, response, data_key, competition_type):
         fixName = lambda name: " ".join(reversed(list(map(str.strip, name.split(",")))))
 
         data = response.json()["data"]
-        for [
-            _id,
-            _rank,
-            name,
-            _team,
-            _nationality,
-            category,
-            _category_detail,
-            _rank_age,
-            _rank_total,
-            raw_duration,
-            _gap,
-            bib,
-        ] in (
-            data[data_key]["#1_M"] + data[data_key]["#2_W"]
-        ):
-            duration = "0" + ("0:%s" % raw_duration.replace(",", "."))[-9:]
+
+        groups = []
+        results = []
+        for team, names in data[data_key].items():
+            [rank_team, team_duration] = re.match(
+                r"\#\d+_(\d+)\.///.*?///(.+)", team
+            ).group(1, 2)
+
+            [
+                [_id1, bib1, name1, duration1, age_group1, _nationality1],
+                [_id2, bib2, name2, duration2, age_group2, _nationality2],
+                [_id3, bib3, name3, duration3, age_group3, _nationality3],
+            ] = names
+
+            team_gender = list(
+                set(map(lambda ag: ag[0].upper(), [age_group1, age_group2, age_group3]))
+            )
+            team_category = team_gender[0] if len(team_gender) == 1 else "X"
 
             yield ResultItem(
                 date=self.race_date,
                 competition_id=self.competition_id,
-                duration=duration,
                 type=competition_type,
-                category=category,
-                names=[fixName(name)],
-                bib=bib,
+                duration=self.fixDuration(team_duration),
+                names=sorted(map(fixName, [name1, name2, name3])),
+                category=team_category,
+                rank=ResultRankItem(total=int(rank_team)),
+                bib=bib1.split("-")[0],
             )
+
+            for bib, name, duration, age_group in zip(
+                [bib1, bib2, bib3],
+                [name1, name2, name3],
+                [duration1, duration2, duration3],
+                [age_group1, age_group2, age_group3],
+            ):
+                category = age_group[0].upper()
+
+                result = ResultItem(
+                    date=self.race_date,
+                    competition_id=self.competition_id,
+                    type=competition_type,
+                    duration=self.fixDuration(duration),
+                    names=[fixName(name)],
+                    category=category,
+                    age_group=age_group,
+                    bib=bib,
+                )
+                results.append(result)
+                groups.append((category, age_group))
+
+        results = sorted(results, key=lambda result: result["duration"])
+
+        for category, age_group in sorted(set(groups)):
+            entries_type = results
+            entries_category = list(
+                filter(lambda result: result["category"] == category, entries_type)
+            )
+            entries_age_group = list(
+                filter(
+                    lambda result: result["age_group"] == age_group, entries_category
+                )
+            )
+
+            for entry in entries_age_group:
+                entry["rank"] = ResultRankItem(
+                    total=entries_type.index(entry) + 1,
+                    category=entries_category.index(entry) + 1,
+                    age_group=entries_age_group.index(entry) + 1,
+                )
+
+                yield entry

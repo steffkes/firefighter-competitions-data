3746364ce7f71623c11e0880b24c03ade24c2ed5
steffkes <stefan@gelungen.es>
2025-02-28 16:23:26 +0100

    apply code format

--- spider/231028_recYTNzFkNBssH8qX_boeblingen.py
+++ spider/231028_recYTNzFkNBssH8qX_boeblingen.py
@@ -4,4 +4,4 @@ from util import FccSpider
 class CompetitionSpider(FccSpider):
     name = __name__
     event_id = 15
-    age_group_fallback = "M" # not sure why this is even needed
+    age_group_fallback = "M"  # not sure why this is even needed

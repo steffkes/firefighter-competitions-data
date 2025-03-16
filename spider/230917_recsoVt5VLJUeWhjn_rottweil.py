40ca9bf906dc0d2549d4298b17946fcdcc7fac8d
steffkes <stefan@gelungen.es>
2025-03-16 06:55:40 +0100

    refactor spider for rottweil
    one for all!

--- spider/230917_recsoVt5VLJUeWhjn_rottweil.py
+++ spider/230917_recsoVt5VLJUeWhjn_rottweil.py
@@ -1,156 +1,16 @@
-import scrapy
-from datetime import datetime
-import requests
-from util import (
-    JsonItemExporter,
-    JsonLinesItemExporter,
-    ParticipantItem,
-    ResultItem,
-    ResultRankItem,
-)
+from util import RottweilSpider
 
 
-class Spider(scrapy.Spider):
+class CompetitionSpider(RottweilSpider):
     name = __name__
-    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
-    competition_id = __name__.split("_")[1]
-    ident = __name__[0:24]
 
     race_id = "233380"
     race_key = "60cc4a4284fd80a91ac2abbea60e10ed"
 
-    custom_settings = {
-        "FEED_EXPORTERS": {
-            "starter": JsonItemExporter,
-            "results": JsonLinesItemExporter,
-        },
-        "FEEDS": {
-            "data/participants/%(ident)s.json": {
-                "format": "starter",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ParticipantItem],
-            },
-            "data/results/%(name)s.jsonl": {
-                "format": "results",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ResultItem],
-            },
-        },
-        "EXTENSIONS": {
-            "scrapy.extensions.telnet.TelnetConsole": None,
-        },
-    }
-
-    def start_requests(self):
-        for contest, data_key in [
-            (5, "#1_{DE:Feuerwehr-Team mit PA|EN:Firefighters team with SCBA}"),
-            (
-                6,
-                "#1_{DE:Feuerwehr-Team ohne PA|EN:Firefighters team without SCBA}",
-            ),
-        ]:
-            yield scrapy.FormRequest(
-                method="GET",
-                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
-                formdata={
-                    "key": self.race_key,
-                    "listname": "07 - Teilnehmer - PUBLIC|Teilnehmer ABC-Team",
-                    "contest": str(contest),
-                },
-                cb_kwargs={"data_key": data_key},
-                callback=self.parse_starters,
-            )
-
-        for contest, competition_type, data_key in [
-            (5, "MPA", "#1_{DE:Feuerwehr-Team mit PA|EN:Firefighters team with SCBA}"),
-            (
-                6,
-                "OPA",
-                "#1_{DE:Feuerwehr-Team ohne PA|EN:Firefighters team without SCBA}",
-            ),
-        ]:
-            r = requests.get(
-                "https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
-                params={
-                    "key": self.race_key,
-                    "listname": "01 - Detail|Details Team",
-                    "contest": str(contest),
-                },
-            )
-
-            yield scrapy.FormRequest(
-                method="GET",
-                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
-                formdata={
-                    "key": self.race_key,
-                    "listname": "02 - Ergebnislisten|Ergebnisliste MW - Team",
-                    "contest": str(contest),
-                },
-                cb_kwargs={
-                    "competition_type": competition_type,
-                    "data_key": data_key,
-                    "details": dict(map(lambda row: (row[0], row), r.json()["data"])),
-                },
-            )
-
-    def parse_starters(self, response, data_key):
-        data = response.json()["data"][data_key]
-        for [_id, _bib, _team, names, _category] in data:
-            yield ParticipantItem(
-                competition_id=self.competition_id,
-                names=sorted(names.split(" / ")),
-            )
-
-    def parse(self, response, data_key, competition_type, details):
-        data = response.json()["data"]
-        for entry in (
-            data[data_key]["#1_Männer"]
-            + data[data_key]["#2_Frauen"]
-            + data[data_key]["#3_Mixed"]
-        ):
-            [id, status, bib, names, age_group, _, raw_duration] = entry
-
-            if status in ["DNF", "a.k."] or not raw_duration:  # disqualified
-                continue
-
-            [category, _] = age_group.split(" ")
-            names = sorted(map(str.strip, names.split("/")))
-            duration = ("0" + ("0:" + raw_duration + ".0")[-9:])[-11:]
-
-            [
-                _id,
-                _bib,
-                _person1,
-                _person2,
-                _team,
-                _org,
-                _unknown,
-                _label1,
-                _label2,
-                _label3,
-                _label4,
-                _contest,
-                _time,
-                _speed,
-                rank_total,
-                rank_category,
-                rank_age_group,
-            ] = details[id]
-
-            yield ResultItem(
-                date=self.race_date,
-                competition_id=self.competition_id,
-                type=competition_type,
-                duration=duration,
-                names=names,
-                category={"MIX": "X"}.get(category, category),
-                age_group=age_group,
-                rank=ResultRankItem(
-                    total=int(rank_total[:-1]),
-                    category=int(rank_category[:-1]),
-                    age_group=int(rank_age_group[:-1]),
-                ),
-                bib=bib,
-            )
+    custom_starter = [
+        (5, "#1_{DE:Feuerwehr-Team mit PA|EN:Firefighters team with SCBA}"),
+        (
+            6,
+            "#1_{DE:Feuerwehr-Team ohne PA|EN:Firefighters team without SCBA}",
+        ),
+    ]
--- spider/util.py
+++ spider/util.py
@@ -1,5 +1,6 @@
 import scrapy
 from datetime import datetime
+import requests
 import re
 
 
@@ -415,6 +416,123 @@ class BadWildbadSpider(Spider):
 
             self.ranks["total"] = rank_total + 1
             self.ranks["category"][category] = rank_category + 1
+
+
+class RottweilSpider(Spider):
+    custom_starter = None
+
+    def start_requests(self):
+        for contest, data_key in self.custom_starter or [(5, None), (6, None)]:
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                formdata={
+                    "key": self.race_key,
+                    "listname": "07 - Teilnehmer - PUBLIC|Teilnehmer ABC-Team",
+                    "contest": str(contest),
+                },
+                cb_kwargs={
+                    "data_key": data_key,
+                },
+                callback=self.parse_starters,
+            )
+
+        for contest, competition_type, data_key in [
+            (5, "MPA", "#1_{DE:Feuerwehr-Team mit PA|EN:Firefighters team with SCBA}"),
+            (
+                6,
+                "OPA",
+                "#1_{DE:Feuerwehr-Team ohne PA|EN:Firefighters team without SCBA}",
+            ),
+        ]:
+            r = requests.get(
+                "https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                params={
+                    "key": self.race_key,
+                    "listname": "01 - Detail|Details Team",
+                    "contest": str(contest),
+                },
+            )
+
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                formdata={
+                    "key": self.race_key,
+                    "listname": "02 - Ergebnislisten|Ergebnisliste MW - Team",
+                    "contest": str(contest),
+                },
+                cb_kwargs={
+                    "competition_type": competition_type,
+                    "data_key": data_key,
+                    "details": dict(map(lambda row: (row[0], row), r.json()["data"])),
+                },
+            )
+
+    def parse_starters(self, response, data_key):
+        data = response.json()["data"]
+
+        if data_key:
+            data = data[data_key]
+
+        for [_id, _id2, _bib, _team, names, _category] in data:
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted(names.split(" / ")),
+            )
+
+    def parse(self, response, data_key, competition_type, details):
+        data = (response.json()["data"] or {}).get(data_key, {})
+        for entry in (
+            data.get("#1_Männer", [])
+            + data.get("#2_Frauen", [])
+            + data.get("#3_Mixed", [])
+        ):
+            [id, _id2, status, bib, names, age_group, _, raw_duration] = entry
+
+            if status in ["DNF", "a.k."] or not raw_duration:  # disqualified
+                continue
+
+            [category, _] = age_group.split(" ")
+            names = sorted(map(str.strip, names.split("/")))
+            duration = ("0" + ("0:" + raw_duration + ".0")[-9:])[-11:]
+
+            [
+                _id,
+                _id2,
+                _bib,
+                _person1,
+                _person2,
+                _team,
+                _org,
+                _unknown,
+                _label1,
+                _label2,
+                _label3,
+                _label4,
+                _contest,
+                _time,
+                _speed,
+                rank_total,
+                rank_category,
+                rank_age_group,
+            ] = details[id]
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type=competition_type,
+                duration=duration,
+                names=names,
+                category={"MIX": "X"}.get(category, category),
+                age_group=age_group,
+                rank=ResultRankItem(
+                    total=int(rank_total[:-1]),
+                    category=int(rank_category[:-1]),
+                    age_group=int(rank_age_group[:-1]),
+                ),
+                bib=bib,
+            )
 
 
 import pytest

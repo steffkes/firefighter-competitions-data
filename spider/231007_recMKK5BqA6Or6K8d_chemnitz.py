0d644a7b6dffee483e21c4c8d00c17a280183b2f
steffkes <stefan@gelungen.es>
2025-03-21 13:37:52 +0100

    add results for chemnitz 2023

--- spider/231007_recMKK5BqA6Or6K8d_chemnitz.py
+++ spider/231007_recMKK5BqA6Or6K8d_chemnitz.py
@@ -1,37 +1,9 @@
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
 import scrapy
-from datetime import datetime, timedelta
-from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
 
 
-class Spider(scrapy.Spider):
+class CompetitionSpider(Spider):
     name = __name__
-    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
-    competition_id = __name__.split("_")[1]
-    ident = __name__[0:24]
-
-    custom_settings = {
-        "FEED_EXPORTERS": {
-            "starter": JsonItemExporter,
-            "results": JsonLinesItemExporter,
-        },
-        "FEEDS": {
-            "data/participants/%(ident)s.json": {
-                "format": "starter",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ParticipantItem],
-            },
-            "data/results/%(name)s.jsonl": {
-                "format": "results",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ResultItem],
-            },
-        },
-        "EXTENSIONS": {
-            "scrapy.extensions.telnet.TelnetConsole": None,
-        },
-    }
 
     def start_requests(self):
         yield scrapy.FormRequest(
@@ -44,21 +16,28 @@ class Spider(scrapy.Spider):
         )
 
     def parse(self, response):
-
         for entry in response.json()["results"]:
-            print(entry)
-
             if entry["time"] in ["DNS", "DNF"]:
                 continue
 
             [minutes, seconds] = entry["time"] // 60, entry["time"] % 60
+            age_group = (
+                {"Männer": "M", "Frauen": "W"}.get(entry["AK"], entry["AK"]).upper()
+            )
+            category = age_group[0]
 
             yield ResultItem(
                 date=self.race_date,
                 competition_id=self.competition_id,
                 duration="00:%02d:%02d.0" % (minutes, seconds),
                 type="MPA",
-                category=entry["AK"][0].upper(),
                 names=["%s %s" % (entry["forename"].strip(), entry["surname"].strip())],
+                category=category,
+                age_group=age_group,
+                rank=ResultRankItem(
+                    total=entry["position"],
+                    category=entry["MW"],
+                    age_group=entry["AKP"],
+                ),
                 bib=entry["bib"],
             )

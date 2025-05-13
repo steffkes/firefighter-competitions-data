16bbe24be62976898ccdb43bbbb5d1eb755930fd
steffkes <stefan@gelungen.es>
2025-05-13 15:00:36 +0200

    add results for radebeul 2025

--- spider/250510_rec3gmKvkR7p38b0I_radebeul.py
+++ spider/250510_rec3gmKvkR7p38b0I_radebeul.py
@@ -1,6 +1,12 @@
 import scrapy
 from datetime import datetime
-from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
+from util import (
+    JsonItemExporter,
+    JsonLinesItemExporter,
+    ParticipantItem,
+    ResultItem,
+    ResultRankItem,
+)
 
 
 class Spider(scrapy.Spider):
@@ -8,6 +14,9 @@ class Spider(scrapy.Spider):
     race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
     competition_id = __name__.split("_")[1]
     ident = __name__[0:24]
+
+    race_id = "339886"
+    race_key = "e94c697fe8e4c5a66536c55ba5d5e5ae"
 
     custom_settings = {
         "FEED_EXPORTERS": {
@@ -40,6 +49,17 @@ class Spider(scrapy.Spider):
             callback=self.parse_starters,
         )
 
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "Feuerwehr|Ergebnis-Online",
+                "contest": "0",
+            },
+            cb_kwargs={"competition_type": "MPA"},
+        )
+
     def parse_starters(self, response):
         num_teams = 60 - int(response.css(".counter::text").get())
         for i in range(num_teams):
@@ -47,3 +67,41 @@ class Spider(scrapy.Spider):
                 competition_id=self.competition_id,
                 names=[None, None],
             )
+
+    def parse(self, response, competition_type):
+        ranks = {"category": {}}
+
+        for _, team in response.json()["data"].items():
+            [
+                [_id1, _id2, bib, name1, _time, raw_duration, category],
+                [_, _, _, name2, _, _, _],
+            ] = team
+
+            names = sorted(
+                [name1, name2],
+            )
+            duration = "00:" + ("0" + raw_duration + ".0")[-7:]
+
+            rank_total = ranks.get("total", 1)
+            rank_category = ranks["category"].get(category, 1)
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type=competition_type,
+                category={
+                    "Männer-Team": "M",
+                    "Mixed-Team": "X",
+                    "Frauen-Team": "W",
+                }.get(category),
+                duration=duration,
+                names=names,
+                rank=ResultRankItem(
+                    total=rank_total,
+                    category=rank_category,
+                ),
+                bib=bib.split("-")[0],
+            )
+
+            ranks["total"] = rank_total + 1
+            ranks["category"][category] = rank_category + 1

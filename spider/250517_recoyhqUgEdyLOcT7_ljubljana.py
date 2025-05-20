5f81046b1942d000a88d26fea73a1fe4d1f72c5c
steffkes <stefan@gelungen.es>
2025-05-20 11:10:23 +0200

    add results for ljubljana 2025

--- spider/250517_recoyhqUgEdyLOcT7_ljubljana.py
+++ spider/250517_recoyhqUgEdyLOcT7_ljubljana.py
@@ -5,6 +5,12 @@ import scrapy
 class CompetitionSpider(Spider):
     name = __name__
 
+    @staticmethod
+    def fixName(name):
+        return " ".join(
+            map(lambda part: part[0].upper() + part[1:].lower(), name.split(" "))
+        )
+
     def start_requests(self):
         yield scrapy.FormRequest(
             method="GET",
@@ -12,10 +18,29 @@ class CompetitionSpider(Spider):
             callback=self.parse_starters,
         )
 
-    def fixName(self, name):
-        return " ".join(
-            map(lambda part: part[0].upper() + part[1:].lower(), name.split(" "))
-        )
+        for contest, type, gender in [
+            ("G1M", "MPA", "M"),  # single male
+            ("G1Z", "MPA", "W"),  # single female
+        ]:
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://remote.timingljubljana.si/stepup/Rezultati.aspx",
+                formdata={"cat": contest},
+                callback=self.parse_single,
+                cb_kwargs={"type": type, "gender": gender},
+            )
+
+        for contest, type in [
+            ("G3", "OPA"),  # relay mixed
+            ("GE", "MPA"),  # relay mixed
+        ]:
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://remote.timingljubljana.si/stepup/Rezultati.aspx",
+                formdata={"cat": contest},
+                callback=self.parse_relay,
+                cb_kwargs={"type": type},
+            )
 
     def parse_starters(self, response):
         for row in response.css("table tr"):
@@ -36,6 +61,56 @@ class CompetitionSpider(Spider):
                 competition_id=self.competition_id,
                 names=[self.fixName("%s %s" % (firstname, lastname))],
             )
+
+    def parse_single(self, response, type, gender):
+        for row in response.css("table tr")[1:]:
+            [rank, name, _country, _team, duration] = row.css("td ::text").getall()
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type=type,
+                duration="0" + duration,
+                names=[self.fixName(name)],
+                category=gender,
+                rank=ResultRankItem(category=int(rank)),
+            )
+
+    def parse_relay(self, response, type):
+        for row in response.css("table tr")[1:]:
+            [
+                rank,
+                names,
+                _country,
+                _team,
+                duration1,
+                duration2,
+                duration3,
+                _sum1_2,
+                relay_duration,
+            ] = row.css("td ::text").getall()
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type=type,
+                duration="0" + relay_duration,
+                names=sorted(map(self.fixName, names.split(","))),
+                category="Relay",
+                rank=ResultRankItem(category=int(rank)),
+            )
+
+            for name, duration in zip(
+                names.split(","), [duration1, duration2, duration3]
+            ):
+                yield ResultItem(
+                    date=self.race_date,
+                    competition_id=self.competition_id,
+                    type=type,
+                    duration="0" + duration,
+                    names=[self.fixName(name.strip())],
+                )
+
 
 
 import pytest

4e25ea1bf33c8b29d345810eb5cc78543f281a83
steffkes <stefan@gelungen.es>
2025-07-03 00:05:35 +0200

    add slots for firefit kassel 2025

--- spider/250802_recPYTDYqzceAPJmV_kassel.py
+++ spider/250802_recPYTDYqzceAPJmV_kassel.py
@@ -9,21 +9,21 @@ class CompetitionSpider(Spider):
     def start_requests(self):
         yield scrapy.FormRequest(
             method="GET",
-            url="https://firefit-europe.eu/en/produkt/firefit-zuerich-einzel-individual-saturday/",
+            url="https://firefit-europe.eu/en/produkt/firefit-kassel-einzel-individual-saturday/",
             callback=self.parse_slots,
             cb_kwargs={"contest": "Einzel"},
         )
 
         yield scrapy.FormRequest(
             method="GET",
-            url="https://firefit-europe.eu/en/produkt/firefit-zuerich-tandem-sunday/",
+            url="https://firefit-europe.eu/en/produkt/firefit-kassel-tandem-sunday/",
             callback=self.parse_slots,
             cb_kwargs={"contest": "Tandem"},
         )
 
         yield scrapy.FormRequest(
             method="GET",
-            url="https://firefit-europe.eu/en/produkt/firefit-zuerich-staffel-relay-sunday/",
+            url="https://firefit-europe.eu/en/produkt/firefit-kassel-staffel-relay-sunday/",
             callback=self.parse_slots,
             cb_kwargs={"contest": "Staffel"},
         )

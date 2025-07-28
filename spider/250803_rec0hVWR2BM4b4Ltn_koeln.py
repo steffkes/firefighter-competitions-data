39e310d8dc968523e9112209b6e762ced6594e28
steffkes <stefan@gelungen.es>
2025-07-28 09:59:47 +0200

    fetch slots for koeln 2025

--- spider/250803_rec0hVWR2BM4b4Ltn_koeln.py
+++ spider/250803_rec0hVWR2BM4b4Ltn_koeln.py
@@ -1,50 +1,18 @@
 import scrapy
-from datetime import datetime
-import re
-import itertools
-import string
-import requests
 from util import (
-    JsonItemExporter,
-    JsonLinesItemExporter,
+    Spider,
     ParticipantItem,
     ResultItem,
     ResultRankItem,
+    SlotItem,
 )
 
 
-class Spider(scrapy.Spider):
+class CompetitionSpider(Spider):
     name = __name__
-    race_date = datetime.strptime(__name__.split("_")[0], "%y%m%d").strftime("%Y-%m-%d")
-    competition_id = __name__.split("_")[1]
-    ident = __name__[0:24]
 
     race_id = "323602"
     race_key = "77bdc85dcb9f1b7a137834310113b943"
-
-    custom_settings = {
-        "FEED_EXPORTERS": {
-            "starter": JsonItemExporter,
-            "results": JsonLinesItemExporter,
-        },
-        "FEEDS": {
-            "data/participants/%(ident)s.json": {
-                "format": "starter",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ParticipantItem],
-            },
-            "data/results/%(name)s.jsonl": {
-                "format": "results",
-                "encoding": "utf8",
-                "overwrite": True,
-                "item_classes": [ResultItem],
-            },
-        },
-        "EXTENSIONS": {
-            "scrapy.extensions.telnet.TelnetConsole": None,
-        },
-    }
 
     def start_requests(self):
         for contest in [5, 6]:
@@ -59,6 +27,12 @@ class Spider(scrapy.Spider):
                 callback=self.parse_starters,
             )
 
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRRegStart/data/config" % self.race_id,
+            callback=self.parse_slots,
+        )
+
     def parse_starters(self, response):
         fixName = lambda name: " ".join(reversed(list(map(str.strip, name.split(",")))))
 
@@ -68,3 +42,10 @@ class Spider(scrapy.Spider):
                 competition_id=self.competition_id,
                 names=sorted(map(fixName, names.split(" / "))),
             )
+
+    def parse_slots(self, response):
+        data = response.json()
+
+        isRegClosed = data["Now"] >= data["RegUntil"]
+        if isRegClosed:
+            yield SlotItem(competition_id=self.competition_id, amount=0)

3073a83a84936e180903876aac5c365016e3e67c
steffkes <stefan@gelungen.es>
2025-08-17 19:20:17 +0200

    add results for schoemberg 2025

--- spider/250801_recTWmxAzBKWWfpvg_schoemberg.py
+++ spider/250801_recTWmxAzBKWWfpvg_schoemberg.py
@@ -1,6 +1,6 @@
 import scrapy
-from datetime import datetime
 from util import Spider, ParticipantItem, ResultItem
+import re
 
 
 class CompetitionSpider(Spider):
@@ -21,6 +21,27 @@ class CompetitionSpider(Spider):
             callback=self.parse_starters,
         )
 
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Baywa und Feuerwehr|Ergebnisliste_Feuerwehr_MW",
+                "contest": "9",
+            },
+        )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Baywa und Feuerwehr|Ergebnisliste_Feuerwehr_Mixed",
+                "contest": "9",
+            },
+            callback=self.parse_mixed,
+        )
+
     def parse_starters(self, response):
         data = list(response.json()["data"].values())[0]
         for [
@@ -39,9 +60,52 @@ class CompetitionSpider(Spider):
                 names=sorted(map(fixName, [name1, name2])),
             )
 
+    def handle_results(self, data, category):
+        for team, entry in data.items():
+            duration = self.fixDuration(team.split("///")[-1])
+            names = sorted(map(lambda record: fixName(record[2]), entry))
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type="OPA",
+                category=category,
+                duration=duration,
+                names=names,
+                bib=None,
+            )
+
+    def parse(self, response):
+        data = response.json()
+
+        yield from self.handle_results(data["data"]["#1_Männlich"], "M")
+        yield from self.handle_results(data["data"]["#2_Weiblich"], "W")
+
+    def parse_mixed(self, response):
+        data = response.json()
+
+        yield from self.handle_results(data["data"], "X")
+
 
 def fixName(name):
-    return " ".join(reversed(list(map(str.strip, name.split(",")))))
+    if "," in name:
+        return " ".join(reversed(list(map(str.strip, name.split(",")))))
+
+    return re.sub(
+        r"(([A-ZÄÜÖäüöß]+[-\s])?([A-ZÄÜÖäüöß]+))\s(.+)",
+        lambda match: " ".join(
+            [
+                match.group(4),
+                "".join(
+                    map(
+                        lambda str: str[0] + str[1:].lower(),
+                        filter(None, match.group(2, 3)),
+                    )
+                ),
+            ]
+        ),
+        name.replace(",", ""),
+    )
 
 
 import pytest
@@ -52,6 +116,8 @@ import pytest
     [
         ("Wetzel, Denise", "Denise Wetzel"),
         ("Julia, Keller", "Keller Julia"),
+        ("TESCHNER-JEIKOWSKI Sarah", "Sarah Teschner-Jeikowski"),
+        ("RAJDA Remigiusz Eugeniusz", "Remigiusz Eugeniusz Rajda"),
     ],
 )
 def test_fixName(input, output):

19acbcd9a138bcf50cc0c1ee958f6a3684ab8eea
steffkes <stefan@gelungen.es>
2025-08-17 15:31:27 +0200

    add results for cologne 2025

--- spider/250803_rec0hVWR2BM4b4Ltn_koeln.py
+++ spider/250803_rec0hVWR2BM4b4Ltn_koeln.py
@@ -6,6 +6,7 @@ from util import (
     ResultRankItem,
     SlotItem,
 )
+import requests
 
 
 class CompetitionSpider(Spider):
@@ -33,6 +34,38 @@ class CompetitionSpider(Spider):
             callback=self.parse_slots,
         )
 
+        for contest, competition_type, data_key in [
+            (5, "MPA", "#1_{DE:Feuerwehr-Team mit PA|EN:Firefighters with SCBA}"),
+            (
+                6,
+                "OPA",
+                "#1_{DE:Feuerwehr-Team ohne PA|EN:Firefighters without SCBA}",
+            ),
+        ]:
+            r = requests.get(
+                "https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                params={
+                    "key": self.race_key,
+                    "listname": "01 - Detail|Details Team",
+                    "contest": str(contest),
+                },
+            )
+
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+                formdata={
+                    "key": self.race_key,
+                    "contest": str(contest),
+                    "listname": "02 - Ergebnislisten|Mannschaftswertung Ges",
+                },
+                cb_kwargs={
+                    "competition_type": competition_type,
+                    "data_key": data_key,
+                    "details": dict(map(lambda row: (row[2], row), r.json()["data"])),
+                },
+            )
+
     def parse_starters(self, response):
         fixName = lambda name: " ".join(reversed(list(map(str.strip, name.split(",")))))
 
@@ -49,3 +82,52 @@ class CompetitionSpider(Spider):
         isRegClosed = data["Now"] >= data["RegUntil"]
         if isRegClosed:
             yield SlotItem(competition_id=self.competition_id, amount=0)
+
+    def parse(self, response, data_key, competition_type, details):
+        for entry in response.json()["data"][data_key]:
+            [_id1, _id2, status, bib, _team, names, age_group, raw_duration] = entry
+
+            # sometimes contains an additional asterik at the end
+            if status[0:4] == "a.k.":  # DNF / DSQ
+                continue
+
+            [category, _] = age_group.split(" ")
+            names = sorted(map(str.strip, names.split("/")))
+            duration = self.fixDuration(raw_duration)
+
+            [
+                _id,
+                _id2,
+                _bib,
+                _person1,
+                _person2,
+                _team,
+                _unknown,
+                _unknown,
+                _label1,
+                _label2,
+                _label3,
+                _label4,
+                _contest,
+                _duration,
+                _speed,
+                rank_total,
+                rank_category,
+                rank_age_group,
+            ] = details[bib]
+
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                duration=duration,
+                type=competition_type,
+                category={"MIX": "X"}.get(category, category),
+                names=names,
+                age_group=age_group,
+                rank=ResultRankItem(
+                    total=int(rank_total[:-1]),
+                    category=int(rank_category[:-1]),
+                    age_group=int(rank_age_group[:-1]),
+                ),
+                bib=bib,
+            )

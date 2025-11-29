a081608ce2257da25a952e189d4dd97b911d118b
steffkes <stefan@gelungen.es>
2025-11-29 19:50:36 +0100

    update participants for zagreb 2025

--- spider/251206_recjK2NA5Tk2hvZfP_zagreb.py
+++ spider/251206_recjK2NA5Tk2hvZfP_zagreb.py
@@ -5,19 +5,31 @@ import scrapy
 class CompetitionSpider(Spider):
     name = __name__
 
-    def start_requests(self):
-        yield scrapy.http.JsonRequest(
+    def requestHelper(self, raceId, page=1):
+        return scrapy.http.JsonRequest(
             method="POST",
             url="https://tracey-prod-6b98eac1c8fad5e7fa6493b8d82e4fc3.traceysport.com/api/public/competitors/find",
-            data={"filter": {"eventId": 2841}, "pages": {"pageSize": 51, "page": 1}},
+            data={
+                "pages": {"pageSize": 51, "page": page},
+                "filter": {"eventId": 2841, "raceId": raceId},
+            },
+                cb_kwargs={"raceId": raceId, "page": page},
             callback=self.parse_starters,
         )
 
-    def parse_starters(self, response):
-        entries = response.json()["result"]
-        for entry in entries[0:50]:
-            if entry["raceId"] in [8051, 8052, 8053]:
-                yield ParticipantItem(
-                    competition_id=self.competition_id,
-                    names=["{} {}".format(entry["firstName"], entry["lastName"])],
-                )
+    def start_requests(self):
+        for raceId in [8052, 8053]:
+            yield self.requestHelper(raceId)
+
+    def parse_starters(self, response, raceId, page):
+        records = response.json()["result"]
+
+        if not records:
+            return
+
+        for record in records:
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=[record["fullName"].strip()],
+            )
+        yield self.requestHelper(raceId, page + 1)

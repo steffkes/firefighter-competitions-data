46a1a9878c078e83d90fff1d91a96fac2587d3d8
steffkes <stefan@gelungen.es>
2026-01-08 00:11:54 +0100

    fetch available slots for ulm

--- spider/260522_recanVcyBiGQJIMUI_ulm.py
+++ spider/260522_recanVcyBiGQJIMUI_ulm.py
@@ -1,4 +1,4 @@
-from util import Spider, ParticipantItem, ResultItem, ResultRankItem
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem, SlotItem
 import scrapy
 
 
@@ -18,6 +18,20 @@ class CompetitionSpider(Spider):
                 "contest": "1",
             },
             callback=self.parse_starters,
+        )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRRegStart/data/config" % self.race_id,
+            callback=self.parse_slots,
+        )
+
+    def parse_slots(self, response):
+        yield SlotItem(
+            competition_id=self.competition_id,
+            amount=response.json()["RegistrationConfig"]["Registrations"][0][
+                "Contests"
+            ][1]["SlotsLeft"],
         )
 
     def parse_starters(self, response):

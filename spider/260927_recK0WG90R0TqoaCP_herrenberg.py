6632556e3a4352e2ab0fe2901b23ad06748a5899
steffkes <stefan@gelungen.es>
2026-02-09 20:44:43 +0100

    add participants for herrenberg 26

---
+++ spider/260927_recK0WG90R0TqoaCP_herrenberg.py
@@ -0,0 +1,153 @@
+from util import (
+    Spider,
+    ParticipantItem,
+    ResultItem,
+    ResultRankItem,
+    JsonLinesItemExporter,
+)
+import scrapy
+import re
+
+
+# we need aggregated results to determine the total rank
+class CustomExporter(JsonLinesItemExporter):
+    def finish_exporting(self):
+        sortedDuration = sorted(map(lambda item: item["duration"], self.items))
+
+        def mapFn(item):
+            item["rank"]["total"] = sortedDuration.index(item["duration"]) + 1
+            return item
+
+        self.items = list(map(mapFn, self.items))
+
+        super().finish_exporting()
+
+
+class CompetitionSpider(Spider):
+    name = __name__
+    race_id = "373019"
+    race_key = "f6e5d91ec4168888a4e945e2ba214d66"
+
+    # extend global spider config
+    custom_settings = Spider.custom_settings
+    custom_settings["FEED_EXPORTERS"]["results"] = CustomExporter
+
+    @staticmethod
+    def fixName(name):
+        return " ".join(reversed(name.split(", ")))
+
+    def start_requests(self):
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "rr Timing|Teilnehmerliste_Feuerwehrlauf",
+                "contest": "0",
+            },
+            callback=self.parse_starters,
+        )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Ergebnisse|Ergebnisliste_Feuerwehr_Mixed",
+                "contest": "1",
+                "f": "Wertung: Fireheroes Leonberg",
+            },
+            cb_kwargs={
+                "category": "X",
+            },
+        )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Ergebnisse|Ergebnisliste_Feuerwehr_MW",
+                "contest": "1",
+                "f": "Wertung: Fireheroes LeonbergMännlich",
+            },
+            cb_kwargs={"category": "M", "key": "#1_Männlich"},
+        )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Ergebnisse|Ergebnisliste_Feuerwehr_MW",
+                "contest": "1",
+                "f": "Wertung: Fireheroes LeonbergWeiblich",
+            },
+            cb_kwargs={"category": "W", "key": "#2_Weiblich"},
+        )
+
+    def parse_starters(self, response):
+        for [
+            _id1,
+            _id2,
+            _origin,
+            name1,
+            name2,
+            _yob1,
+            _yob2,
+            _gender,
+        ] in ((response.json()["data"] or {}).get("#1_Feuerwehrlauf")) or []:
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=sorted([name1, name2]),
+            )
+
+    def parse(self, response, category, key=None):
+        data = (
+            (response.json()["data"] or {}).get("#1_Wertung: Fireheroes Leonberg")
+        ) or {}
+
+        if key:
+            data = data.get(key, {})
+
+        items = []
+
+        for team, results in data.items():
+            [rank_category, raw_duration] = re.match(
+                r"\#\d+_(\d+)\.///.*?///(.+)", team
+            ).group(1, 2)
+
+            items.append(
+                ResultItem(
+                    date=self.race_date,
+                    competition_id=self.competition_id,
+                    type="OPA",
+                    duration=self.fixDuration(raw_duration),
+                    # results = [ [_, _, name, _, _], [_, _, name, _, _] ]
+                    names=sorted(map(lambda result: self.fixName(result[2]), results)),
+                    category=category,
+                    rank=ResultRankItem(),
+                )
+            )
+
+        sortedDuration = sorted(map(lambda item: item["duration"], items))
+
+        def mapFn(item):
+            item["rank"]["category"] = sortedDuration.index(item["duration"]) + 1
+            return item
+
+        yield from list(map(mapFn, items))
+
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        ("Schimmer, Astrid", "Astrid Schimmer"),
+        ("Bartholomä, Meike", "Meike Bartholomä"),
+    ],
+)
+def test_fixName(input, output):
+    assert CompetitionSpider.fixName(input) == output

a533b7ae12839098ec8844f37720b315a2a49fc4
steffkes <stefan@gelungen.es>
2026-03-22 20:21:41 +0100

    add participants for magdeburg 2026

---
+++ spider/260429_recE7U1cQtUxmhOyd_magdeburg.py
@@ -0,0 +1,22 @@
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
+import scrapy
+
+
+class CompetitionSpider(Spider):
+    name = __name__
+
+    def start_requests(self):
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://www.berlin-timing.de/Liste/54cf88cceb98b07469fe3fb9d505ec44.csv",
+            callback=self.parse_starters,
+        )
+
+    def parse_starters(self, response):
+        for row in scrapy.utils.iterators.csviter(
+            "\n".join(response.text.split("\n")[3:]), delimiter=";"
+        ):
+            yield ParticipantItem(
+                competition_id=self.competition_id,
+                names=["{} {}".format(row["Vorname"], row["Nachname"])],
+            )

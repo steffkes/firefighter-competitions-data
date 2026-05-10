a00348fa34bcd86f5e774c8fe02a2b5f12cb2b34
steffkes <stefan@gelungen.es>
2026-05-10 21:14:29 +0200

    add results for quinto 2026

--- spider/260509_recQpiFds3jkOXxU2_quinto.py
+++ spider/260509_recQpiFds3jkOXxU2_quinto.py
@@ -1,5 +1,7 @@
 from util import Spider, ParticipantItem, ResultItem, ResultRankItem
 import scrapy
+import json
+import re
 
 
 class CompetitionSpider(Spider):
@@ -12,6 +14,28 @@ class CompetitionSpider(Spider):
             formdata={"idevento": "100485", "rows": "200", "page": "1"},
             callback=self.parse_starters,
         )
+
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://egs-eventi.s3.amazonaws.com/00000000/00100000/00100400/00100485/results/jsonp/615003_5GHUV40FU7.jsonp",
+        )
+
+    @staticmethod
+    def loads_jsonp(text):
+        text = text.strip()
+
+        # remove optional comment prefix
+        text = re.sub(r"^/\*\*/", "", text)
+
+        start = text.find("(")
+        end = text.rfind(")")
+
+        if start == -1 or end == -1:
+            raise ValueError("Invalid JSONP")
+
+        payload = text[start + 1 : end]
+
+        return json.loads(payload)
 
     def parse_starters(self, response):
         for entry in response.json()["rows"]:
@@ -29,3 +53,28 @@ class CompetitionSpider(Spider):
                 competition_id=self.competition_id,
                 names=["%s %s" % (firstname, lastname)],
             )
+
+    def parse(self, response):
+        results = []
+
+        for row in self.loads_jsonp(response.text)["rows"]:
+            category = {"M": "M", "F": "W"}[row["sx"]]
+            duration = self.fixDuration(row["tu"])
+
+            results.append(
+                ResultItem(
+                    date=self.race_date,
+                    competition_id=self.competition_id,
+                    type="OPA",
+                    duration=duration,
+                    names=["%s %s" % (row["nn"], row["nc"])],
+                    category=category,
+                    rank=ResultRankItem(
+                        total=None,
+                        category=None,
+                    ),
+                )
+            )
+
+        for result in results:
+            yield result

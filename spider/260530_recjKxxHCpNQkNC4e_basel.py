4f14cb0a6a6b2c42f8c2f368f4ae6ed6a24bd6fd
steffkes <stefan@gelungen.es>
2026-05-30 22:51:46 +0200

    add results for basel 2026

---
+++ spider/260530_recjKxxHCpNQkNC4e_basel.py
@@ -0,0 +1,56 @@
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
+import scrapy
+
+
+class CompetitionSpider(Spider):
+    name = __name__
+
+    @staticmethod
+    def fixName(name):
+        parts = name.split(" ")
+        parts.append(parts.pop(0))
+        return " ".join(parts)
+
+    def start_requests(self):
+        for page in range(1, 4):
+            yield scrapy.FormRequest(
+                method="GET",
+                url="https://results.datasport.com/api",
+                formdata={
+                    "task": "ranking",
+                    "edition": "tower-run-basel2026",
+                    "slug": "mixt-in-4er-teams",
+                    "lang": "de",
+                    "page": str(page),
+                },
+            )
+
+    def parse(self, response):
+        for team in response.json()["table"]["teams"]:
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type="MPA",
+                duration=self.fixDuration(team["data"]["time.main"]["text"]),
+                names=sorted(
+                    map(lambda row: self.fixName(row["name"]["text"]), team["rows"])
+                ),
+                rank=ResultRankItem(
+                    total=team["data"]["rank.main"],
+                ),
+            )
+
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        ("Woischnig Anne-Kathrin", "Anne-Kathrin Woischnig"),
+        ("Volger Natalie Patricia", "Natalie Patricia Volger"),
+        ("Birmes Lukas", "Lukas Birmes"),
+    ],
+)
+def test_fixName(input, output):
+    assert CompetitionSpider.fixName(input) == output

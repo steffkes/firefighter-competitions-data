88ebc9b4394da3efb01ba5fd7e0f513fac709495
steffkes <stefan@gelungen.es>
2026-07-01 16:15:57 +0200

    add participants for oberhof 2026/12

--- spider/261205_rec3CZhk0vSiVwpkM_oberhof.py
+++ spider/261205_rec3CZhk0vSiVwpkM_oberhof.py
@@ -6,15 +6,27 @@ from util import (
     ResultRankItem,
     SlotItem,
 )
+from itertools import batched
 
 
 class CompetitionSpider(Spider):
     name = __name__
 
     race_id = "394956"
-    race_key = ""
+    race_key = "5536f414f74accaa71b17159b889072f"
 
     def start_requests(self):
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/participants/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "Online|Teilnehmer",
+                "contest": "0",
+            },
+            callback=self.parse_starters,
+        )
+
         yield scrapy.FormRequest(
             method="GET",
             url="https://my.raceresult.com/%s/registration/config" % self.race_id,
@@ -26,3 +38,14 @@ class CompetitionSpider(Spider):
             competition_id=self.competition_id,
             amount=response.json()["Registrations"][0]["Contests"][0]["SlotsLeft"],
         )
+
+    def parse_starters(self, response):
+        for item in response.json()["data"]["#1_StairRun - Feuerwehr"].values():
+            for [
+                [_id11, _id12, name1, _team1, _nat1, _yob1],
+                [_id21, _id22, name2, _team2, _nat2, _yob2],
+            ] in batched(item, 2):
+                yield ParticipantItem(
+                    competition_id=self.competition_id,
+                    names=sorted(map(self.fixName, [name1, name2])),
+                )

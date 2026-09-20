1ea8dcc19ba2f85bfb733e1f0d8194cf9dd6c066
steffkes <stefan@gelungen.es>
2026-09-20 21:48:57 +0200

    update montafon treppencup 2025

--- spider/250913_recloHjA7XxWa1hpC_partenen.py
+++ spider/250913_recloHjA7XxWa1hpC_partenen.py
@@ -6,39 +6,31 @@ class CompetitionSpider(Spider):
     name = __name__
 
     def start_requests(self):
-        # dont_filter is needed, because otherwise the
-        # second request is treated as duplicate for
-        # the first one ..
-
-        yield scrapy.Request(
-            "data:,",
-            dont_filter=True,
-            callback=self.parse_starters,
-        )
-
+        yield scrapy.Request("data:,", dont_filter=True, callback=self.parse_starters)
         yield scrapy.Request("data:,", dont_filter=True)
 
     def parse_starters(self, response):
-        for name in ["Nicolas Faure", "Stefan Matheis", "Elias Vogt", "Marcel König"]:
-            yield ParticipantItem(
-                competition_id=self.competition_id,
-                names=[name],
-            )
+        for name in ["Stefan Matheis", "Nicolas Faure", "Marcel König", "Elias Vogt"]:
+            yield ParticipantItem(competition_id=self.competition_id, names=[name])
 
     def parse(self, response):
-        for total_rank, raw_duration, name in [
-            (1, "1:07:11.29", "Stefan Matheis"),
-            (2, "1:12:47.09", "Marcel König"),
-            (3, "1:23:29.81", "Nicolas Faure"),
-        ]:
+        rank_category = {"M": 1, "W": 1}
+        for rank_total, (raw_duration, category, name) in enumerate(
+            [
+                ("01:07:11.29", "M", "Stefan Matheis"),
+                ("01:23:29.81", "M", "Marcel König"),
+                ("01:12:47.09", "M", "Nicolas Faure"),
+            ]
+        ):
             yield ResultItem(
                 date=self.race_date,
                 competition_id=self.competition_id,
                 type="OPA",
                 duration=self.fixDuration(raw_duration),
                 names=[name],
-                category="M",
-                age_group=None,
-                bib=None,
-                rank=ResultRankItem(total=total_rank),
+                category=category,
+                rank=ResultRankItem(
+                    total=rank_total + 1, category=rank_category[category]
+                ),
             )
+            rank_category[category] += 1

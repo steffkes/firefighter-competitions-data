897b36171246bd86654a0fabc04b7bbe09bcff05
steffkes <stefan@gelungen.es>
2026-09-20 21:51:35 +0200

    add montafon bergcup 2025

---
+++ spider/250914_rec4NwSr8YwhdUYfl_partenen.py
@@ -0,0 +1,37 @@
+from util import Spider, ParticipantItem, ResultItem, ResultRankItem
+import scrapy
+
+
+class CompetitionSpider(Spider):
+    name = __name__
+
+    def start_requests(self):
+        yield scrapy.Request("data:,", dont_filter=True, callback=self.parse_starters)
+        yield scrapy.Request("data:,", dont_filter=True)
+
+    def parse_starters(self, response):
+        for name in ["Stefan Matheis", "Marcel König", "Nicolas Faure", "Elias Vogt"]:
+            yield ParticipantItem(competition_id=self.competition_id, names=[name])
+
+    def parse(self, response):
+        rank_category = {"M": 1, "W": 1}
+        for rank_total, (raw_duration, category, name) in enumerate(
+            [
+                ("00:46:20.03", "M", "Stefan Matheis"),
+                ("00:47:33.30", "M", "Marcel König"),
+                ("00:50:48.92", "M", "Nicolas Faure"),
+                ("00:59:25.67", "M", "Elias Vogt"),
+            ]
+        ):
+            yield ResultItem(
+                date=self.race_date,
+                competition_id=self.competition_id,
+                type="OPA",
+                duration=self.fixDuration(raw_duration),
+                names=[name],
+                category=category,
+                rank=ResultRankItem(
+                    total=rank_total + 1, category=rank_category[category]
+                ),
+            )
+            rank_category[category] += 1
